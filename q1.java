cc5908ebcce62fb372f414bfcf2af809323cfbbb
maham qadeer <maham.qadeer2003@gmail.com>
2023-10-08 17:47:59 +0500

    added stack class

--- q1.java
+++ q1.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.EmptyStackException;
+
 class node <T> {
     public T data;
     public node <T> next;
@@ -23,6 +26,61 @@ class node <T> {
         return next;
     }
     
+}
+class stack <T>{
+    public int size;
+    public node<T> top;
+
+    public stack (){
+        size=0;
+        top=null;
+    }
+    public stack (int size, node<T> top){
+        this.size=size;
+        this.top=top;
+    }
+    public int getSize(){
+        return size;
+    }
+    public boolean isEmpty(){
+        if(getSize()==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public void push(T data){
+        if(!isEmpty()){
+            node<T> temp = new node<>(data);
+            top=temp;
+            size--;
+        }
+        else{
+             throw new StackEmptyException ("Stack is empty!");
+        }
+    }
+    public void pop(){
+        if (!isEmpty()) {
+            T data= top.getData();
+            top=top.getNext();
+            size--;
+            System.out.println("Popped element: "+ data);
+        }
+        else{
+            throw new StackEmptyException ("Stack is empty!");
+        }
+    }
+    public void peek(){
+         if (!isEmpty()) {
+            System.out.println("Peek element: "+ top);
+        }
+        else{
+            throw new StackEmptyException ("Stack is empty!");
+        }
+    }
+
+
 }
 public class q1{
     public static void main(String[] args){
