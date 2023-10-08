1ace268316a2c054f390b5396f977f6b4d9f6321
maham qadeer <maham.qadeer2003@gmail.com>
2023-10-08 17:07:33 +0500

    added node class

--- q1.java
+++ q1.java
@@ -1,3 +1,29 @@
+class node <T> {
+    public T data;
+    public node <T> next;
+
+    public node(){
+        data = null;
+        next=null;
+    }
+    public node(T data){
+        this.data = data;
+        next=null;
+    }
+    public void setData(T data){
+        this.data=data;
+    }
+    public void setNext(node<T> next){
+        this.next=next;
+    }
+    public T getData(){
+        return data;
+    }
+    public node<T> getNext(){
+        return next;
+    }
+    
+}
 public class q1{
     public static void main(String[] args){
         System.out.println("Hello Stack");
